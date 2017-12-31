26beef1700cf9346543dee600b332fafd38d9572
Milok <milok.murlika@yandex.ua>
2017-12-31 14:25:05 +0200

    Доверстал страницу с таблицей

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,13 @@
 $('.phone-mask').mask('+7 (000) 000 - 00 - 00');
+
+$('.touch-menu__item').click(function() {
+	$(this).toggleClass('touch-menu__item_active');
+});
+
+$('.top-menu:first-child .top-menu__button').click(function() {
+	$(this).next().toggleClass("hidden")
+});
+
+$('.dropdown__item').click(function() {
+	$(this).toggleClass('dropdown__item_active');
+});
